41ead373bd0e95a81e694a387bf9af65a622da7a
Jeneliya12 <gurungjeneliya@gmail.com>
2024-10-07 15:48:21 -0400

    initialize react with tailwin configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl bg-red-500">Tailwind Configuration Test</h1>
+      <p className="mt-4">
+        This is a simple test to verify custom configurations!
+      </p>
+    </div>
+  );
+}
+
+export default App;
